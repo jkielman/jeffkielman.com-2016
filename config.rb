afe0b04ca7376680e611d08bae859a5be484951f
jkielman <jeffkielman@Jeffs-MacBook-Pro.local>
2016-09-29 00:45:18 -0400

    First Commit 🎆

---
+++ config.rb
@@ -0,0 +1,42 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+
+page "/index*", :layout => "index"
+
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+activate :directory_indexes
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+activate :minify_css
+activate :minify_javascript
+activate :minify_html, remove_input_attributes: false
+
+
+
+
+end
